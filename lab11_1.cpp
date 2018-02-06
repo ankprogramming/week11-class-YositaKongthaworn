dfe999f393ba06bcba442d020b230764d1e85b3d
Unknown <sodomyoindy@gmail.com>
2018-02-06 11:54:55 +0700

    เสร้จ

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -7,11 +7,17 @@ class Student
 {
 private:
     int id;
-    // เพิ่มเอาเอง
+    string name;
+    int age;
+    double heg;
+
 public:
     Student(int i, string n, int a, double h)
     {
         id = i;
+        name =n;
+        age =a;
+        heg =h;
 
     }
 
@@ -19,11 +25,51 @@ public:
     {
         return id;
     }
-    // เพิ่มเอาเอง
-};
+    string getName ()
+    {
+        return name;
+    }
+    int getAge ()
+    {
+        return age;
+    }
+    double getHeg()
+    {
+        return heg ;
+    }
 
+};
 int main ()
 {
+    Student Noctis(36548,"Noctis",20,176);
+    Student Ignis(36549,"Ignis",22,183);
+    Student gladioius(36550,"gladioius",23,198);
+    Student prompto(36551,"prompto",20,173);
+
+    cout << " ID : " << Noctis.getId() <<endl;
+    cout << " Name : " << Noctis.getName() <<endl;
+    cout << " Age : " << Noctis.getAge() <<endl;
+    cout << " Height : " << Noctis.getHeg() <<endl;
+
+
+    cout << " ID : " << Noctis.getId() <<endl;
+    cout << " Name : " << Noctis.getName() <<endl;
+    cout << " Age : " << Noctis.getAge() <<endl;
+    cout << " Height : " << Noctis.getHeg() <<endl;
+
+
+    cout << " ID : " << Noctis.getId() <<endl;
+    cout << " Name : " << Noctis.getName() <<endl;
+    cout << " Age : " << Noctis.getAge() <<endl;
+    cout << " Height : " << Noctis.getHeg() <<endl;
+
+
+    cout << " ID : " << Noctis.getId() <<endl;
+    cout << " Name : " << Noctis.getName() <<endl;
+    cout << " Age : " << Noctis.getAge() <<endl;
+    cout << " Height : " << Noctis.getHeg() <<endl;
+
+
 
     return 0;
 }
